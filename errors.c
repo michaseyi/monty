b95f058dba6eac701602a3680e28d3b2aa10c873
Adewole Michael <michaseyi@gmail.com>
2022-06-16 01:03:24 +0100

    First test

--- errors.c
+++ errors.c
@@ -16,25 +16,25 @@ void exit_errors(int type)
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", filename);
 		break;
 	case INVALID_OP:
-		dprintf(STDERR_FILENO, "L%d: unknown instruction %s\n", line, input[0]);
+		dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line, input[0]);
 		break;
 	case MALLOC_ERR:
 		dprintf(STDERR_FILENO, "Error: malloc failed\n");
 		break;
 	case PUSH_ERR:
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line);
+		dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line);
 		break;
 	case POP_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't pop an empty stack\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't pop an empty stack\n", line);
 		break;
 	case PINT_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't pint an empty stack\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't pint an empty stack\n", line);
 		break;
 	case SWAP_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't swap, stack too short\n", line);
 		break;
 	case ADD_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line);
 		break;
 	}
 	exit_errors_2(type);
@@ -64,25 +64,25 @@ void exit_errors_2(int type)
 	switch (type)
 	{
 	case SUB_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't sub, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line);
 		break;
 	case DIV_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't div, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line);
 		break;
 	case DIV_BY_ZERO:
-		dprintf(STDERR_FILENO, "L%d: division by zero\n", line);
+		dprintf(STDERR_FILENO, "L%u: division by zero\n", line);
 		break;
 	case MUL_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't mul, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't mul, stack too short\n", line);
 		break;
 	case MOD_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't mod, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't mod, stack too short\n", line);
 		break;
 	case PCHAR_ERR:
-		dprintf(STDERR_FILENO, "L%d: can't pchar, stack too short\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't pchar, stack too short\n", line);
 		break;
 	case PCHAR_OUT_OF_RANGE:
-		dprintf(STDERR_FILENO, "L%d: can't pchar, value out of range\n", line);
+		dprintf(STDERR_FILENO, "L%u: can't pchar, value out of range\n", line);
 		break;
 	}
 	clean_up();
